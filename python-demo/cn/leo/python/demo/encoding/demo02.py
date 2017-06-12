c7720e36201a32a4780a582d4aa4c7271eaba516
cyliang1113 <cyliang1113@126.com>
2017-06-12 15:42:42 +0800

    1111

--- python-demo/cn/leo/python/demo/encoding/demo02.py
+++ python-demo/cn/leo/python/demo/encoding/demo02.py
@@ -1,13 +1,13 @@
 # coding: utf-8
 
-str1 = u'中国'
+str1 = u"中国"
 print str1
 print type(str1)
 print len(str1)
 
 print "================================="
 
-str2 = '中国'
+str2 = "中国"
 print str2
 print type(str2)
 print len(str2)
