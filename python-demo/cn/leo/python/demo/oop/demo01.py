c70c43cea1bdd636f558125534664d49c4079242
cyliang1113 <cyliang1113@126.com>
2017-06-20 13:56:48 +0800

    1111111111

--- python-demo/cn/leo/python/demo/oop/demo01.py
+++ python-demo/cn/leo/python/demo/oop/demo01.py
@@ -3,12 +3,16 @@
 class Person(object):  # 括号中表示该类的父类，如果没有自定义父类，一般用object
     
     # 和普通的函数相比，在类中定义的函数只有一点不同，就是第一个参数永远是实例变量self，并且，调用时，不用传递该参数。
-    def __init__(self, name):
+    def __init__(self, name, sex="man"):
         self.name = name
+        self.__sex = sex
      
     # 方法就是与实例绑定的函数，和普通函数不同，方法可以直接访问实例的数据；   
     def getName(self):
         return self.name
+    
+    def getSex(self):
+        return self.__sex
     
 
 p = Person(u"leo")
@@ -18,4 +22,8 @@ print p.getName()
 
 p.age = 15
 print p.age
+
+# print p.__sex    __开头的对象属性不能直接访问, 会报错.
+print p.getSex()
+print dir(p)
 
--- python-demo/cn/leo/python/demo/web/flask/demo01.py
+++ python-demo/cn/leo/python/demo/web/flask/demo01.py
@@ -5,21 +5,21 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["get", "post"])
+@app.route("/", methods=["get"])
 def home():
     return render_template("home.html")
 
-@app.route("/loginPage", methods=["get", "post"])
+@app.route("/login/page", methods=["get"])
 def loginPage():
     return render_template("loginPage.html")
 
-@app.route("/login", methods=["get", "post"])
+@app.route("/login", methods=["post"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
+    username = request.form.get("username")
+    password = request.form.get("password")
     if("admin" == username and "admin" == password):
         return "login success"
     return render_template("loginPage.html", message='Bad username or password', username=username)
 
-if __name__ == "__main__":
-    app.run("", 8080)
+if __name__ == "__main       __":
+    app.run("", 8080, debug=True)
