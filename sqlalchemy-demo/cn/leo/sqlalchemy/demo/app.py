db65ccb80f9959effcff768d68ab709135c09bb5
cyliang1113 <cyliang1113@126.com>
2017-06-23 10:26:33 +0800

    11111

--- sqlalchemy-demo/cn/leo/sqlalchemy/demo/app.py
+++ sqlalchemy-demo/cn/leo/sqlalchemy/demo/app.py
@@ -4,15 +4,15 @@ from cn.leo.sqlalchemy.demo.database.db import get_new_session
 
 from cn.leo.sqlalchemy.demo.orm.models import UserUser
 
-session1 = get_new_session()
-print session1
+session = get_new_session()
+print session
 
-user_user_list = session1.query(UserUser).all()
+user_user_list = session.query(UserUser).all()
 
 for user_user in user_user_list:
     print user_user.user_id, user_user.user_name
 
-session1.close()
+session.close()
 
 
 
