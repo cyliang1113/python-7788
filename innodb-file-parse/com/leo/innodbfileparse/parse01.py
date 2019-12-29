79a2448a4eb6faf42cd70ba3df0d061fe986adb3
cyliang1113 <cyliang1113@126.com>
2019-12-29 18:14:33 +0800

    1111

--- innodb-file-parse/com/leo/innodbfileparse/parse01.py
+++ innodb-file-parse/com/leo/innodbfileparse/parse01.py
@@ -7,11 +7,13 @@ def pageNo(file, offset):
     data = file.read(4)
     print("页编号: " + data.hex())
 
+
 def beforePage(file, offset):
     s = offset + 8;
     file.seek(s)
     data = file.read(4)
     print("前一页编号: " + data.hex())
+
 
 def afterPage(file, offset):
     s = offset + 12;
@@ -20,7 +22,29 @@ def afterPage(file, offset):
     print("后一页编号: " + data.hex())
 
 
-file_name = 'C:/Users/youliang.chen/Desktop/test1/user.ibd'
+def pageType(file, offset):
+    s = offset + 24;
+    file.seek(s)
+    data = file.read(2)
+    key = (data.hex());
+
+    map = {
+        "45bf": "B+树叶子节点页",
+        "0002": "undo log页",
+        "0003": "索引节点页",
+        "0004": "insert buffer空闲列表页",
+        "0000": "最新分配页",
+        "0005": "insert buffer位图页",
+        "0006": "系统页",
+        "0007": "事务系统数据页",
+        "0008": "File Space Header页",
+        "0009": "扩展描述页",
+        "000a": "BLOB页",
+    }
+    print("页类型: " + key + ", " + str(map.get(key)));
+
+
+file_name = 'C:/Users/leon/Desktop/test1/user_user.ibd'
 page_size = 16;  # 页大小16K
 size = int(path.getsize(file_name) / 1024)
 page_count = int(size / page_size)
@@ -35,3 +59,4 @@ with open(file_name, "rb") as file:
         pageNo(file, offset)
         beforePage(file, offset)
         afterPage(file, offset)
+        pageType(file, offset)
